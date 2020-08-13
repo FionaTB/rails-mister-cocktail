4298210956e212ded6bc0a83dd8d6b7fe86fb3cf
Fiona Toublanc <fionatoublanc@gmail.com>
2020-08-13 15:52:32 +0200

    2. seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Dose.destroy_all
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+Ingredient.create(name: "mint")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "lemon juice")
+Ingredient.create(name: "sugar syrup")
+Ingredient.create(name: "soda water")
+Ingredient.create(name: "cayenne pepper")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "lime juice")
+Ingredient.create(name: "blackberry syrup")
+
+puts 'Creating cocktail'
